619af6f9d3af43ed16a7ac9e0b32321626210a7a
Dhruval Patil <dhruvalpatil2248@gmail.com>
2026-09-12 16:30:08 +0530

    resolve merge conflicts permentaly

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,5 @@
 print ("Hello World")
-<<<<<<< HEAD
 print ("this in VS code")
-=======
 print ("my proctice")
 print ("feature branch")
 print ("Hello from feature Branch")
->>>>>>> 041c277adb3f0d32bfe1ffd742e4d8dbcfb0ae79
